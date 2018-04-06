672756f01b88bfea5d4e443eb7a996655c01832b
Dany_ <Dany_@DESKTOP-EETTRI2>
2018-04-06 16:09:56 -0600

    Tarea de laboratorio 2

---
+++ src/tarea_labo2/Tarea_Labo2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tarea_labo2;
+
+/**
+ *
+ * @author Luisa Arevalo <00038617@uca.edu.sv>
+ */
+public class Tarea_Labo2 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
